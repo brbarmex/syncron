60b457ebcea242834bd7f2b67b188bfd2e53afe1
Bruno Melo <bruno.b.melo@live.com>
2023-09-10 22:52:31 -0300

    feat: merge imports from cron.rs

--- src/cron.rs
+++ src/cron.rs
@@ -1,7 +1,4 @@
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
+use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, thread, time::Duration};
 
 use crate::service;
 
